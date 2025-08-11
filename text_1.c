94419fd18672949eff38de3b46efe198b7207fc3
Choco-Man <abcdesp32@gmail.com>
2025-08-11 23:00:37 +0530

    Created text_1.c and text_2.c

---
+++ text_1.c
@@ -0,0 +1,13 @@
+// **** _ UnderScore as a Variable Name **** //
+
+#include <stdio.h>
+
+int main(){
+
+    int _ = 10;
+
+    if ( _ == 1) 
+        printf("ONE");
+    else 
+        printf("Other than ONE");
+}
---
+++ text_2.c
@@ -0,0 +1,8 @@
+// **** Printf() as a Variable Name **** //
+
+#include <stdio.h>
+
+int main(){
+
+    printf(" - %d Chars", printf("Prakadeesh"));
+}
